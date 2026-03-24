7bd54afca3279a34bca95e4d10c7d9a26cd739c0
DESKTOP-GJ77IJ9\tuand <tuandd.gplaystudio@gmail.com>
2026-03-24 18:06:19 +0700

    fix build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,6 @@ const config: CapacitorConfig = {
   appId: "com.gcognify.tik.die.tok.death.clock",
   appName: "Death Clock",
   webDir: "dist",
-  server: {
-    androidScheme: "http",
-  },
   plugins: {
     Keyboard: {
       resize: "none",
